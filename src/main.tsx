271b16f6722ecd5c53c52e36a7dd45ecc4834c03
zsol36 <kronomezsolt36@gmail.com>
2026-01-07 21:30:06 +0100

    c

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <BrowserRouter basename="ForMore">
     <App />
   </BrowserRouter>
 )
